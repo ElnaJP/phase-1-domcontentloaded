1d8f212700e860512d240e9de515bb4669cd3242
Elna JP <eriosdd@gmail.com>
2022-08-04 16:00:08 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+ /*function DOMContentLoaded() {
+    document.querySelector('p').textContent;
+    console.log( "JavaScript is so cool. It lets me add text to my page programmatically.");
+}
+ ;
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+const event = document.createEvent( 'Event');
+function createEvent() {
+     document.querySelector('p').textContent;
+     console.log("This is really cool!");*/
+  
+     document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("text").innerHTML= "This is really cool!"
+      });  
+
